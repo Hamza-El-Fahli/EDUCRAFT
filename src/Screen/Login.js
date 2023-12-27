445d298b5541badb6cd565cb5c81ee28d00ec237
Hamza <hamzaelfahli4@gmail.com>
2023-12-27 23:39:58 +0100

    collect all styles in ./src/Styles/styles , make register UI

--- src/Screen/Login.js
+++ src/Screen/Login.js
@@ -1,15 +1,19 @@
 
 import { useNavigation } from '@react-navigation/native'
 import React from 'react'
-import { Pressable, StyleSheet, View, Text,Box, TextInput, Button } from 'react-native'
+import { Pressable, View, Text, TextInput, Image } from 'react-native'
+import logo from "../images/1b.png"
+import styles from '../Styles/styles'
 
-const Home = () => {
+const Login = () => {
   const navigation = useNavigation()
 
   return (
     <View>
       <View >
-              <Text style={styles.title}>EDUCRAFT</Text>
+      <Image 
+              source={logo}
+              style={styles.logo}/>
               <Text style={styles.login}>Login</Text>
                 <Text style={styles.loginLabel}>
                   Email :
@@ -32,7 +36,7 @@ const Home = () => {
         onPress={()=> navigation.navigate("Main")}
         style={styles.loginButton}
       >
-        <Text style={styles.loginButtonText}>CREATE AN ACCOUNT</Text>
+        <Text style={styles.loginButtonText}>LOGIN TO EDUCRAFT</Text>
       </Pressable>
 
           <Pressable
@@ -65,77 +69,4 @@ const Home = () => {
     </View>
   )
 }
-export default Home
-
-const styles = StyleSheet.create({
-  title : {
-    textAlign: 'center',
-    fontSize : 32,
-    fontWeight : 'bold',
-    marginTop :10,
-  },
-  login : {
-    textAlign: 'center',
-    fontSize : 32,
-    fontWeight : 'bold',
-    marginTop : 10,
-    marginBottom : 60
-  },
-  inputEmail : {
-    marginLeft : 30,
-    width : '80%',
-    marginTop : 15,
-    // paddingTop : 5,
-    height : 25,
-    borderBottomWidth : 1,
-    borderBottomColor : '#1F233A'
-  },
-  inputPassword : {
-    marginLeft : 30,
-    width : '80%',
-    marginTop : 10,
-    // paddingTop : 5,
-    height : 25,
-    borderBottomWidth : 1,
-    borderBottomColor : '#1F233A'
-  },
-  loginLabel : {
-    marginLeft : 30,
-    fontSize : 24,
-    marginTop : 10,
-    fontWeight : 'bold'
-  },
-  loginButton: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingVertical: 12,
-    paddingHorizontal: 32,
-    borderRadius: 4,
-    backgroundColor: '#1F233A',
-    height: 60,
-    width : 300,
-    marginLeft : 'auto',
-    marginRight : 'auto',
-    marginTop : 80,
-  },
-  loginButtonText: {
-    fontSize: 13,
-    lineHeight: 21,
-    fontWeight: 'bold',
-    letterSpacing: 0.25,
-    color: '#E9E8F1',
-  },
-  loginGoToRegister : {
-    marginTop:32,
-    marginLeft : 'auto',
-    marginRight : 'auto',
-
-  },
-  loginForgotPassword : {
-        marginTop: 32,
-        textAlign : 'right',
-        fontSize : 13,
-      marginRight : 10,
-      fontWeight : 'bold'
-  }
-})
+export default Login
---
+++ src/Styles/styles.js
@@ -0,0 +1,86 @@
+import { StyleSheet } from "react-native";
+
+
+
+
+
+
+
+const styles = StyleSheet.create({
+    logo : {
+      marginTop :10,
+      marginLeft : 'auto',
+      marginRight : 'auto',
+      height:84,
+      width:247,
+      resizeMode: 'contain', // Set the desired resizeMode
+  
+    },
+    login : {
+      textAlign: 'center',
+      fontSize : 32,
+      fontWeight : 'bold',
+      marginTop : 10,
+      marginBottom : 60
+    },
+    inputEmail : {
+      marginLeft : 30,
+      width : '80%',
+      marginTop : 15,
+      // paddingTop : 5,
+      height : 25,
+      borderBottomWidth : 1,
+      borderBottomColor : '#1F233A'
+    },
+    inputPassword : {
+      marginLeft : 30,
+      width : '80%',
+      marginTop : 10,
+      // paddingTop : 5,
+      height : 25,
+      borderBottomWidth : 1,
+      borderBottomColor : '#1F233A'
+    },
+    loginLabel : {
+      marginLeft : 30,
+      fontSize : 24,
+      marginTop : 10,
+      fontWeight : 'bold'
+    },
+    loginButton: {
+      alignItems: 'center',
+      justifyContent: 'center',
+      paddingVertical: 12,
+      paddingHorizontal: 32,
+      borderRadius: 4,
+      backgroundColor: '#1F233A',
+      height: 60,
+      width : 300,
+      marginLeft : 'auto',
+      marginRight : 'auto',
+      marginTop : 80,
+    },
+    loginButtonText: {
+      fontSize: 13,
+      lineHeight: 21,
+      fontWeight: 'bold',
+      letterSpacing: 0.25,
+      color: '#E9E8F1',
+    },
+    loginGoToRegister : {
+      marginTop:32,
+      marginLeft : 'auto',
+      marginRight : 'auto',
+  
+    },
+    loginForgotPassword : {
+          marginTop: 32,
+          textAlign : 'right',
+          fontSize : 13,
+        marginRight : 10,
+        fontWeight : 'bold'
+    }
+  })
+
+
+  export default styles
