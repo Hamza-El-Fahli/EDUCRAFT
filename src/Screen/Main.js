fa1a8f832645500ddb796d55c9fe6309abafe1b9
Hamza <hamzaelfahli4@gmail.com>
2024-01-01 22:20:21 +0100

    Main screen basic structure

--- src/Screen/Main.js
+++ src/Screen/Main.js
@@ -1,14 +1,45 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, Pressable, StyleSheet, Text, TextInput, View } from 'react-native'
 import React from 'react'
 
 const Main = () => {
   return (
-    <View>
-      <Text>main</Text>
+    <View style={styles.body}>
+      <View style={styles.nav}>
+          <Pressable style={styles.humberger}>
+
+          </Pressable>
+          <Image style={styles.logo} />
+          <Pressable style={styles.profilePic} />
+      </View>
+      <View style={styles.welcome} >
+        <Text style={styles.textH1} >Hello , Hamza</Text>
+        <Text style={styles.textH2} >Welcome back to your course</Text>
+      </View>
+      <View style={styles.search} >
+          <TextInput  placeholder='Search by course, instructor....'/>
+      </View>
+      <View style={styles.filter} >
+        <Text style={{...styles.textH1,...styles.textBold}} >
+            Category
+        </Text>
+        <Pressable>
+           <Text> View all</Text>
+        </Pressable>
+      </View>
     </View>
   )
 }
 
 export default Main
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  body : {
+    backgroundColor : '#17172D',
+
+  },
+  nav  : {},
+  humberger : {},
+  profilePic : {},
+
+
+})
