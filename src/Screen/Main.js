817929347e5c8d6d0a2fa94d17ff91a46c4a1f66
Hamza <hamzaelfahli4@gmail.com>
2024-01-06 14:19:47 +0100

    add some styling to Main

--- src/Screen/Main.js
+++ src/Screen/Main.js
@@ -1,5 +1,7 @@
 import { Image, Pressable, StyleSheet, Text, TextInput, View } from 'react-native'
 import React from 'react'
+import { FontAwesome } from '@expo/vector-icons'
+import proPic from '../images/1b.png'
 
 const Main = () => {
 
@@ -31,36 +33,40 @@ const Main = () => {
     <View style={styles.body}>
       <View style={styles.nav}>
           <Pressable style={styles.humberger}>
-
+            <FontAwesome name='plus-square' size={24}/>
           </Pressable>
+          <Pressable style={styles.profilePic}>
           <Image style={styles.logo} />
-          <Pressable style={styles.profilePic} />
+          </Pressable>
       </View>
       <View style={styles.welcome} >
         <Text style={styles.textH1} >Hello , Hamza</Text>
         <Text style={styles.textH2} >Welcome back to your course</Text>
       </View>
       <View style={styles.search} >
-          <TextInput  placeholder='Search by course, instructor....'/>
+          <TextInput  placeholder='Search by course, instructor....'
+          placeholderTextColor={'gray'}
+          style={{padding : 5,paddingLeft : 10,height:'100%',width:'100%'}}
+          />
       </View>
       <View style={styles.filter} >
         <Text style={{...styles.textH1,...styles.textBold}} >
             Category
         </Text>
         <Pressable>
-           <Text> View all</Text>
+           <Text style={{color : "#4680B9"}}> View all</Text>
         </Pressable>
       </View>
       <View style={styles.courses} >
           {Cources.map((elem)=>
-                  <View key={Math.random()}>
-                  <Text>
+                  <View style={{width : 180 ,height : 80 , borderWidth : 2, marginBottom : 10 }} key={Math.random()}>
+                  <Text style={{position : 'absolute' , top : 20,  left : 80}}>
                     {elem.name}
                   </Text>
-                    <Text>
+                    <Text style={{position : 'absolute' , top : 40, left : 80}}>
                       {elem.lectures} Lectures
                     </Text>
-                    <Text>
+                    <Text style={{position : 'absolute' , top : 20, left : 20}}>
                       {elem.logo}
                     </Text>
                   </View>)}
@@ -76,9 +82,52 @@ const styles = StyleSheet.create({
     backgroundColor : '#17172D',
 
   },
-  nav  : {},
-  humberger : {},
+  nav  : {
+    display : 'flex',
+    flexDirection : 'row',
+    backgroundColor : 'gray',
+    justifyContent : 'space-between',
+    padding : 10 ,
+  },
+  humberger : { },
   profilePic : {},
-
+  logo : {
+    backgroundColor : 'white',
+    height : 20,
+    width : 20,
+  },
+  welcome : {
+    padding : 10,
+    marginTop : 10,
+    marginBottom : 10,
+  },
+  textH1 : {
+    color : 'white',
+    fontSize: 28,
+    fontWeight : 'bold',
+  },
+  textH2 : {
+    color : 'white',
+    fontSize: 22,
+  },
+  search : {
+    borderWidth : 3,
+    borderColor : 'white',
+    borderRadius : 10,
+    margin : 10,
+    height : 50,
+  },
+  filter : {
+    flexDirection : 'row',
+    alignItems : 'center',
+    justifyContent : 'space-between',
+    margin : 10,
+  },
+  courses : {
+    display : 'flex',
+    flexDirection : 'row',
+    justifyContent : 'space-around',
+    flexWrap: 'wrap',
+  },
 
 })
