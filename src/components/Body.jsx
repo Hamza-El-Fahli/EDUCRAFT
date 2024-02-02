55095e4fefcf352b55e0f0c4118b1df2487f40ad
hamza-el-fahli <hamzaelfahli4@gmail.com>
2024-02-03 00:41:27 +0100

    fix: display data in slides

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -7,31 +7,31 @@ const Body =  ({ pageNumber , setPageNumber, progression}) => {
   const data = [
     { id:0,
       title : 'Section 1 : Modèle OSI',
-      progression : '100'
+      progression : 100
   },
   { id:1,
     title : 'Section 2 : Modèle     TCP/IP',
-    progression : '80'
+    progression : '30'
 },
 { id:2,
   title : 'Section 3 : LAN Switching Technologies',
-  progression : '80'
+  progression : 80
 },
 { id:3,
   title : 'Section 4 : Routing Technologies',
-  progression : '80'
+  progression : 0
 },
 { id:4,
   title : 'Section 5 : WAN Technologies',
-  progression : '80'
+  progression : 20
 },
 { id:5,
   title : 'Section 6 : Infrastructure Services',
-  progression : '80'
+  progression : 100
 },
 { id:6,
   title : 'Section 7 : Infrastructure Security',
-  progression : '80'
+  progression : 50
 },
   
   ]
@@ -55,7 +55,7 @@ const Body =  ({ pageNumber , setPageNumber, progression}) => {
     <View style={styles.nav_btns}>
           {data.map((item)=>{
             if(item.id == pageNumber) return (<Pressable  onPress={()=>{setPageNumber(item.id)}} key={item.id}><Text style={{...styles.nav_btn,...styles.nav_btn_selected}}>.</Text></Pressable>)
-            return (<Text style={styles.nav_btn} onPress={()=>{setPageNumber(item.id) ; console.log(data[item.id])}} key={item.id}>.</Text>)
+            return (<Text style={styles.nav_btn} onPress={()=>{setPageNumber(item.id)}} key={item.id}>.</Text>)
           })}
     </View>
 </View>
@@ -66,31 +66,35 @@ const Slides = ({ pageNumber  , data })=>{
 return (
   
   <View style={styles.slide} >
-  {pageNumber == 0 && <View style={styles.slide_item} >
+   <View style={styles.slide_item} >
     <Text style={styles.slide_item_title} >{data[pageNumber].title}</Text>
     <Pressable ><Text style={styles.slide_item_details}>VOIR DETAILS</Text></Pressable>
-    <View >
+    
+    
+    { 
+    data[pageNumber].progression === 100 &&
+     <View >
          <Text style={styles.slide_item_progression}>✔Completed!</Text>
     </View>
+    }
+
+      {
+        data[pageNumber].progression < 100 &&
+        <View 
+        style={styles.progression_bar_bg} 
+        key='progression_bar'
+        >
+        <View 
+          style={{...styles.progression_bar,width : data[1].progression+'%'}}
+          />
+        </View>
+      }
+
       <Pressable style={styles.slide_item_btn_grp}>
         <Text style={styles.slide_item_btn}>REVIEW</Text>
       </Pressable>
-  </View>}
-  {pageNumber != 0 &&<View style={styles.slide_item} >
-    <Text style={styles.slide_item_title} >{data[1].title}</Text>
-    <Pressable ><Text style={styles.slide_item_details}>VOIR DETAILS</Text></Pressable>
-    <View 
-      style={styles.progression_bar_bg} 
-      key='progression_bar'
-      >
-      <View 
-        style={{...styles.progression_bar,width : data[1].progression+'%'}}
-        />
-      </View>
-      <Pressable style={styles.slide_item_btn_grp}>
-        <Text style={styles.slide_item_btn}>REVIEW</Text>
-      </Pressable>
-  </View>}
+  </View>
+  
 </View>
 
 )
