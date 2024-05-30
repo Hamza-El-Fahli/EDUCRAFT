0451cd80d4f3902b9008dfb5df68c625a82457a1
Hamza-El-Fahli <hamzaelfahli4@gmail.com>
2024-05-30 12:51:51 +0100

    wip: ShowResults screen - add go back button

--- src/Screens/quizes/ShowResults.js
+++ src/Screens/quizes/ShowResults.js
@@ -1,19 +1,20 @@
-import {ActivityIndicator, StyleSheet, Text, View} from 'react-native';
-import React, {useEffect, useState} from 'react';
-import {useSelector , useDispatch} from 'react-redux';
+import { ActivityIndicator, Pressable, StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { setChapters, setModules } from '../../store/courseSlice';
 import styles from '../../styles/quizesStyle';
-import {useNavigation} from '@react-navigation/native';
+import { useNavigation } from '@react-navigation/native';
 import axios from 'axios';
-import {Next_Quizzes_Check} from '../../GlobalConfig';
+import { Next_Quizzes_Check } from '../../GlobalConfig';
+import Colors from '../../styles/colors';
 const ShowResults = () => {
   const answers = useSelector(state => state.quiz.answers);
-const dispatch = useDispatch()
+  const dispatch = useDispatch()
   const chapter_id = useSelector(state => state.quiz.chapter_id);
   const quizGroup = useSelector(state => state.quiz.selectedGroup);
   const module_id = useSelector(state => state.course.selectedModule_id);
   const user_id = useSelector(state => state.user.user._id);
-  const chapters = useSelector(state=>state.course.chapters)
+  const chapters = useSelector(state => state.course.chapters)
   const modules = useSelector(state => state.course.module)
 
   const navigation = useNavigation();
@@ -33,12 +34,12 @@ const dispatch = useDispatch()
       .then(res => {
         setTestedAnswers(res.data);
         if (res.data.every(answer => answer.isCorrect)) {
-          const newChaptersState = chapters.map((chapter)=>{
-            if(chapter._id == chapter_id) return {...chapter , isDone : chapter.isDone+1}
+          const newChaptersState = chapters.map((chapter) => {
+            if (chapter._id == chapter_id) return { ...chapter, isDone: chapter.isDone + 1 }
             return chapter
           })
-          const newModulesState = modules.map((_module)=>{
-            if(_module._id == module_id) return {..._module , done : _module.done+1 , progress : (_module.done+1)*100 / _module.chapter_count}
+          const newModulesState = modules.map((_module) => {
+            if (_module._id == module_id) return { ..._module, done: _module.done + 1, progress: (_module.done + 1) * 100 / _module.chapter_count }
             return _module
           })
           dispatch(setChapters(newChaptersState))
@@ -55,9 +56,19 @@ const dispatch = useDispatch()
     <ActivityIndicator />
   ) : (
     <View style={styles.container}>
+
+      <Pressable onPress={() => navigation.navigate("Chapters")}
+        style={{
+          position: 'absolute', bottom: 0, height: 50, width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', alignSelf: 'center',
+          backgroundColor: Colors.Dark.lightBlue, borderTopLeftRadius: 15, borderTopRightRadius: 15
+        }}
+      >
+        <Text style={{ color: Colors.Dark.white, fontSize: 20, fontWeight: 'bold' }}>Go Back</Text>
+      </Pressable>
+
       {testedAnswers.map((answer, index) => {
         return (
-          <Text key={index} style={{color: answer.isCorrect ? 'green' : 'red'}}>
+          <Text key={index} style={{ color: answer.isCorrect ? 'green' : 'red' }}>
             Q{index + 1}: {answer.quiz_question}
           </Text>
         );
